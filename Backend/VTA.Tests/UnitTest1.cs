e68d85c8797eeb7d1122ae68197e0741c5ea3357
Emilprivate <emilsprojects@gmail.com>
2024-10-03 15:31:43 +0200

    Restructured bits. Added the VTA project, with inner projects consisting of VTA.API and VTA.Tests. VTA.Tests uses VTA.API as a project reference. Added Frontend folder with a vta_app flutter project. It uses a the industry standard template so we can follow a similar approach when developing the project.

---
+++ Backend/VTA.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace VTA.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Frontend/vta_app/android/app/src/main/kotlin/com/example/vta_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vta_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
