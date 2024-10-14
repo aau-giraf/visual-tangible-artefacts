7e57bbcab929d20df0bf1f4bc0329f325ab02734
Malario1999 <mscarp22@student.aau.dk>
2024-10-14 22:46:58 +0200

    added gesturedetector to TalkingMat so that it can handle placement

--- Frontend/vta_app/lib/src/ui/widgets/board/artifact.dart
+++ Frontend/vta_app/lib/src/ui/widgets/board/artifact.dart
@@ -4,22 +4,10 @@ class Artifact {
   final Widget content;
   Offset position;
   final GlobalKey key; // GlobalKey for accessing the widget's context
-  final double height; // Height of the artifact
-  final double width; // Width of the artifact
-  Size? _renderedSize; // Private property to store the rendered size
+  Size? renderedSize; // Private property to store the rendered size
 
   Artifact({
     required this.content,
     required this.position,
-    required this.height,
-    required this.width,
-  }) : key = GlobalKey(); // Initialize the GlobalKey
-
-  // Getter for rendered size
-  Size? get renderedSize => _renderedSize;
-
-  // Method to update the rendered size (can be called internally)
-  set renderedSize(Size? size) {
-    _renderedSize = size;
-  }
+  }) : key = GlobalKey();
 }
