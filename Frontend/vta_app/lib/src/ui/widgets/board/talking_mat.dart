aef9440a83c5e0c0d9da2cd4dc5932a38fde8c6a
mLario <mscarp22@student.aau.dk>
2024-10-17 09:59:14 +0200

    added correct artifact placement checking

--- Frontend/vta_app/lib/src/ui/widgets/board/talking_mat.dart
+++ Frontend/vta_app/lib/src/ui/widgets/board/talking_mat.dart
@@ -21,7 +21,6 @@ class TalkingMat extends StatefulWidget {
 
 class TalkingMatState extends State<TalkingMat> {
   late List<Artifact> artifacts;
-  Size? renderedMatSize;
   bool isGestureInsideMat = false;
 
   @override
@@ -48,7 +47,7 @@ class TalkingMatState extends State<TalkingMat> {
       // Offset the position by half of the width and height to center it on the drag end
       setState(() {
         artifact.position =
-            Offset(offset.dx - size.width / 2, offset.dy - size.height / 2);
+            Offset(offset.dx - size.width / 4, offset.dy - size.height / 4);
       });
     }
   }
@@ -67,110 +66,112 @@ class TalkingMatState extends State<TalkingMat> {
     });
   }
 
-  void _loadMatSize() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final RenderBox? renderBox = context.findRenderObject() as RenderBox?;
-      if (renderBox != null) {
-        renderedMatSize = renderBox.size;
-      }
-    });
-  }
+  bool _isInsideMat(Offset globalOffset) {
+    final RenderBox? renderBox = context.findRenderObject() as RenderBox?;
+    if (renderBox == null) return false;
 
-  bool _isInsideMat(Offset localOffset) {
-    return localOffset.dx >= 0 &&
-        localOffset.dx <= (renderedMatSize?.width ?? 0) &&
-        localOffset.dy >= 0 &&
-        localOffset.dy <= (renderedMatSize?.height ?? 0);
+    // Get the global position of the top-left corner of the TalkingMat
+    final Offset matTopLeftGlobal = renderBox.localToGlobal(Offset.zero);
+
+    // Check if the artifact is inside the mat by comparing global coordinates
+    return globalOffset.dx - matTopLeftGlobal.dx >= 0 &&
+        globalOffset.dx + matTopLeftGlobal.dx <= renderBox.size.width &&
+        globalOffset.dy - matTopLeftGlobal.dy >= 0 &&
+        globalOffset.dy + matTopLeftGlobal.dy <= renderBox.size.height;
   }
 
   @override
   Widget build(BuildContext context) {
-    _loadMatSize();
-    return GestureDetector(
-      onLongPressDown: (details) {
-        isGestureInsideMat = true;
-        print("long press init");
-      },
-      onLongPressCancel: () {
-        isGestureInsideMat = false;
-        print("long press cancelled");
-      },
-      child: Container(
-        height: widget.height,
-        width: widget.width,
-        decoration: BoxDecoration(
-          color: widget.backgroundColor ?? Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Stack(children: [
-          ...artifacts.map((artifact) {
-            // Call the function to get the size of the artifact once
-            _loadArtifactSize(artifact);
-            return Positioned(
-              left: artifact.position.dx,
-              top: artifact.position.dy,
-              child: Draggable<Artifact>(
-                data: artifact,
-                feedback: Transform.scale(
-                  scale: 1.2,
-                  child: PhysicalModel(
-                    color: Colors.black,
-                    elevation: 20.0,
-                    shape: BoxShape.circle,
-                    child: artifact.content,
-                  ),
-                ),
-                childWhenDragging: Container(),
-                child: Container(key: artifact.key, child: artifact.content),
-                onDragEnd: (details) {
-                  var localOffset = (context.findRenderObject() as RenderBox)
-                      .globalToLocal(details.offset);
-                  if (isGestureInsideMat) {
-                    _updateArtifactPosition(artifact, details.offset);
-                  }
-                },
-              ),
-            );
-          }),
-          Align(
-            alignment:
-                Alignment.lerp(Alignment.bottomCenter, Alignment.center, 0.1) ??
-                    Alignment.bottomCenter,
-            child: DragTarget<Artifact>(
-              builder: (context, data, rejectedData) {
-                return Container(
-                  width: 50,
-                  height: 50,
-                  decoration: const ShapeDecoration(
-                    color: Color(0xFFF0F2D9),
-                    shape: OvalBorder(),
-                    shadows: [
-                      BoxShadow(
-                        color: Color(0x3F000000),
-                        blurRadius: 4,
-                        offset: Offset(0, 4),
-                        spreadRadius: 0,
-                      )
-                    ],
-                  ),
-                  child: Center(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/icons/trash_bin.png'),
-                              fit: BoxFit.scaleDown)),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          height: widget.height ?? constraints.maxHeight,
+          width: widget.width ?? constraints.maxWidth,
+          decoration: BoxDecoration(
+            color: widget.backgroundColor ?? Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
+            children: [
+              ...artifacts.map((artifact) {
+                _loadArtifactSize(
+                    artifact); // Ensure the artifact size is captured
+                return Positioned(
+                  left: artifact.position.dx,
+                  top: artifact.position.dy,
+                  child: Draggable<Artifact>(
+                    data: artifact,
+                    feedback: Transform.scale(
+                      scale: 1.2,
+                      child: PhysicalModel(
+                        color: Colors.black,
+                        elevation: 20.0,
+                        shape: BoxShape.circle,
+                        child: artifact.content,
+                      ),
                     ),
+                    childWhenDragging: Container(),
+                    child:
+                        Container(key: artifact.key, child: artifact.content),
+                    onDragEnd: (details) {
+                      if (_isInsideMat(details.offset)) {
+                        _updateArtifactPosition(artifact, details.offset);
+                      }
+                    },
                   ),
                 );
-              },
-              onAcceptWithDetails: (details) {
-                var artifactKey = details.data.key;
-                removeArtifact(artifactKey);
-              },
-            ),
+              }),
+              Align(
+                alignment: Alignment.lerp(
+                        Alignment.bottomCenter, Alignment.center, 0.1) ??
+                    Alignment.bottomCenter,
+                child: DragTarget<Artifact>(
+                  builder: (context, data, rejectedData) {
+                    return Stack(children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const ShapeDecoration(
+                          color: Color(0xFFF0F2D9),
+                          shape: OvalBorder(),
+                          shadows: [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                              spreadRadius: 0,
+                            )
+                          ],
+                        ),
+                        child: Center(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/icons/trash_bin.png'),
+                                fit: BoxFit.scaleDown,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]);
+                  },
+                  onAcceptWithDetails: (details) {
+                    var artifactKey = details.data.key;
+                    removeArtifact(artifactKey);
+                  },
+                  onWillAccept: (data) {
+                    setState(() {
+                      // Set hovering state
+                    });
+                    return true;
+                  },
+                ),
+              ),
+            ],
           ),
-        ]),
-      ),
+        );
+      },
     );
   }
 }
