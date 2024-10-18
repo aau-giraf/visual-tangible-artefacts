4f6b4ec5d37fe00e30364195212d76b168663ec6
svedballe <jhan22@student.aau.dk>
2024-10-18 13:05:55 +0200

    Relation button, linear button and linear page

--- Frontend/vta_app/lib/src/ui/widgets/board/linear_board.dart
+++ Frontend/vta_app/lib/src/ui/widgets/board/linear_board.dart
@@ -13,80 +13,119 @@ class LinearBoard extends StatelessWidget {
         title: const Text('Linear Board'),
         automaticallyImplyLeading: false, // Remove the back arrow
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Center( // Background box container
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.9, // Adjust width as needed
+          height: MediaQuery.of(context).size.height * 0.6, // Adjust height as needed
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 2,
+                offset: const Offset(0, 4),
+              )
+            ]
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildBox(context, 'Box 1'),
+              _buildVerticalDivider(context),
+              _buildBox(context, 'Box 2'),
+              _buildVerticalDivider(context),
+              _buildBox(context, 'Box 3'),
+              _buildVerticalDivider(context),
+              _buildBox(context, 'Box 4'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Align(
+        alignment: const Alignment(-0.9, -0.8), // Adjust the alignment as needed
+        child: const LinearBoardButton(), // Use the LinearBoardButton widget
+      ),
+    );
+  }
+
+  Widget _buildBox(BuildContext context, String title) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          _showCategories(context, title);
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    color: Colors.grey[300],
-                    child: const Center(child: Text('Box 1')),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.height * 0.4,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            const VerticalDivider(
-              width: 1,
-              thickness: 1,
-              color: Colors.black,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    color: Colors.grey[300],
-                    child: const Center(child: Text('Box 2')),
+                  child: IconButton(
+                    icon: const Icon(Icons.add, color: Colors.white),
+                    onPressed: () {
+                      _showCategories(context, title);
+                    },
                   ),
-                ],
-              ),
-            ),
-            const VerticalDivider(
-              width: 1,
-              thickness: 1,
-              color: Colors.black,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    color: Colors.grey[300],
-                    child: const Center(child: Text('Box 3')),
-                  ),
-                ],
-              ),
-            ),
-            const VerticalDivider(
-              width: 1,
-              thickness: 1,
-              color: Colors.black,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    color: Colors.grey[300],
-                    child: const Center(child: Text('Box 4')),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: const Align(
-        alignment: Alignment(-0.9, -0.8), // Adjust the alignment as needed
-        child: LinearBoardButton(), // Use the LinearBoardButton widget
-      ),
+    );
+  }
+
+  Widget _buildVerticalDivider(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.44,
+      width: 1,
+      color: Colors.black,
+    );
+  }
+
+   void _showCategories(BuildContext context, String title) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Wrap(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height * 0.5,
+                maxWidth: MediaQuery.of(context).size.width,
+              ),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Center(
+                child: Text('Categories for $title'),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
