2ef54aec56085e33e39a2a64eb59a44fa45ccbff
PG2W <kartofleadama@gmail.com>
2024-10-28 14:45:59 +0100

    Update artifact_board_screen.dart

--- Frontend/vta_app/lib/src/ui/screens/artifact_board_screen.dart
+++ Frontend/vta_app/lib/src/ui/screens/artifact_board_screen.dart
@@ -1,8 +1,66 @@
-@override
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:vta_app/src/models/artefact.dart';
+import 'package:vta_app/src/models/category.dart';
+import 'package:vta_app/src/models/user.dart';
+import 'package:vta_app/src/ui/widgets/board/artifact.dart';
+import 'package:vta_app/src/ui/widgets/board/talking_mat.dart';
+import 'package:vta_app/src/utilities/api/api_provider.dart';
+import '../widgets/board/relational_board_button.dart';
+import '../widgets/board/linear_board.dart';
+import '../widgets/board/quickchat.dart';
+import '../widgets/categories/categories_widget.dart';
+
+class ArtifactBoardScreen extends StatefulWidget {
+  const ArtifactBoardScreen({super.key});
+
+  @override
+  State<ArtifactBoardScreen> createState() => _ArtifactBoardScreenState();
+}
+
+class _ArtifactBoardScreenState extends State<ArtifactBoardScreen> {
+  bool _showDirectional = false;
+  late TalkingMat talkingMat;
+  late GlobalKey<TalkingMatState> talkingMatKey;
+  late LinearBoard linearBoard;
+  List<Category> categories = List.generate(
+      10,
+      (int index) => Category(
+            categoryId: index.toString(),
+            categoryIndex: index,
+            name: "Category $index",
+            // artifacts: List.generate(10, (int index) => Artefact(
+            //                     artefactId: index.toString(),
+            //                     imageUrl: "https://random.imagecdn.app/500/150",
+            //                     // imageUrl: "https://st3.depositphotos.com/2212674/16303/i/450/depositphotos_163039262-stock-photo-outraged-woman-asking-what-the.jpg",
+            //                     // position: const Offset(299, 200),
+            //                   ),
+            artifacts: List.generate(
+                10,
+                (int index) => Artifact(
+                      content: SvgPicture.asset('assets/icons/sillyface.svg'),
+                      position: const Offset(299, 200),
+                    )),
+          ));
+
+  @override
+  void initState() {
+    super.initState();
+    talkingMatKey = GlobalKey<TalkingMatState>();
+    talkingMat = TalkingMat(
+      key: talkingMatKey,
+      artifacts: [],
+    );
+    linearBoard = LinearBoard();
+  }
+
+  @override
   Widget build(BuildContext context) {
     double padding = 10;
     double screenHeight = MediaQuery.of(context).size.height;
     double categoriesWidgetHeight = 120;
+    double dividerHeight = 20;
 
     return Scaffold(
       body: Container(
@@ -22,7 +80,8 @@
               child: Container(
                 height: screenHeight -
                     categoriesWidgetHeight -
-                    MediaQuery.of(context).padding.top - dividerHeight,
+                    MediaQuery.of(context).padding.top -
+                    dividerHeight,
                 padding: EdgeInsets.symmetric(horizontal: padding),
                 child: Stack(
                   children: [
