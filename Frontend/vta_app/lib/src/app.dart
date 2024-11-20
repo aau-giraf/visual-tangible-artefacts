9ed3c417e3590ba03390bb03a6c21a3abfdf7f42
mLario <mscarp22@student.aau.dk>
2024-11-20 09:32:31 +0100

    added rudementary settings page

--- Frontend/vta_app/lib/src/app.dart
+++ Frontend/vta_app/lib/src/app.dart
@@ -56,9 +56,9 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+          // theme: ThemeData(),
+          // darkTheme: ThemeData.dark(),
+          // themeMode: ThemeMode.light,
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
@@ -74,6 +74,7 @@ class MyApp extends StatelessWidget {
                   case SampleItemListView.routeName:
                   default:
                     return AuthPage();
+                    return SettingsView(controller: settingsController);
                 }
               },
             );
