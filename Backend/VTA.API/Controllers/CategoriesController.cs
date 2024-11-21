92cb1ef8e477e9e177656a5f37c311f112cfd097
Kragemand <rasmusdamsboe@gmail.com>
2024-11-21 14:11:12 +0100

    aa

--- Backend/VTA.API/Controllers/CategoriesController.cs
+++ Backend/VTA.API/Controllers/CategoriesController.cs
@@ -106,8 +106,9 @@ public class CategoriesController : ControllerBase
     }
 
     // POST: api/Categories
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754 
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
+    [RequestSizeLimit(200_000_000)]//20mb (Greater than an 8K image) 
     public async Task<ActionResult<CategoryGetDTO>> PostCategory([FromForm] CategoryPostDTO categoryPostDTO)
     {
         var userId = User.FindFirst("id")?.Value;
