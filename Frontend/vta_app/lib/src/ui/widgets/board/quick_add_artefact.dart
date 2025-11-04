24192b0300dc4114861e3cde777048edd8b3bbef
Sakarias <Sakariasmoelgaard@gmail.com>
2025-11-04 13:12:13 +0100

    WIP QuickAddArtefact functionality

---
+++ Frontend/vta_app/lib/src/ui/widgets/board/quick_add_artefact.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:vta_app/src/ui/widgets/board/add_item_popup.dart';
+import 'package:vta_app/src/controllers/artifact_controller.dart';
+
+// For now just refer to already used menues, but maybe add its own functionality later
+
+class QuickAddArtefactButton extends StatefulWidget {
+
+  final ArtefactController artefactController;
+
+  const QuickAddArtefactButton({
+    super.key,
+    required this.artefactController,
+  });
+
+  @override
+  State<QuickAddArtefactButton> createState() => _FloatingActionButtonExampleState();
+}
+
+class _FloatingActionButtonExampleState extends State<QuickAddArtefactButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50.0, right: 120.0),
+            child: FloatingActionButton(
+              heroTag: 'quickAddArtefact',
+              onPressed: () {
+                widget.artefactController.newArtifact(context, 'QUICK_ADDED_ARTEFACT');
+              },
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.green,
+              shape: const CircleBorder(),
+              child: const Icon(Icons.add_rounded, size: 40),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
