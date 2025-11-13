717b99f2e157c8d6d7e672bed4e6199c7ecf0b80
MagnusHK3 <magnus.h.kristensen@gmail.com>
2025-11-13 08:53:51 +0100

    bloat text

--- mysql_schema.sql
+++ mysql_schema.sql
@@ -120,6 +120,3 @@ CREATE TABLE savedArtefact (
   DEFAULT CHARSET=utf8mb4
   COLLATE=utf8mb4_0900_ai_ci;
 
--- Schema creation complete
--- Note: The artefactIds and savedArtefactIds JSON columns will be populated 
--- automatically by the application when boards are created/updated
