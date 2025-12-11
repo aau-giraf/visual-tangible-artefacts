dce8a4090f4041baf17b3d83a8988875f7578dcc
Kasper Verner <mail@kasperverner.com>
2025-12-11 09:54:05 +0100

    moved syncservice to backend and fixed docker compose issues

--- Backend/SyncService/Models/ArtifactAdded/ArtifactAddedPayload.cs
+++ Backend/SyncService/Models/ArtifactAdded/ArtifactAddedPayload.cs
@@ -3,5 +3,5 @@ namespace SyncService.Models.ArtifactAdded;
 public class ArtifactAddedPayload
 {
     public required string SessionId { get; set; }
-    public ArtifactPayload Artifact { get; set; }
+    public required ArtifactPayload Artifact { get; set; }
 }
